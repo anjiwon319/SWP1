08d258c05605b6ec9ae092ad2daac2b737129f6c
anjiwon319 <anjiwon319@kookmin.ac.kr>
2020-06-28 05:54:02 +0900

    issue #7 modified structure & detailed name

--- TEMPLATE.py
+++ TEMPLATE.py
@@ -1,13 +1,13 @@
-html = b'''
+html = '''
 <html>
     <body>
         <form action="">
-            A = <input type="number" name="a"><br><br>
-            B = <input type="number" name="b"><br><br>
+                A = <input type="number" name="a"><br><br>
+                B = <input type="number" name="b"><br><br>
             <input type="submit">
-            <br><br> Add = %(Add)s<br>
-            <br><br> Multiply = %(Multiply)s<br>
         </form>
+        <br><br>Add(A+B) = %(add)d <br><br>
+        Multiply(A*B) = %(mul)d
     </body>
 </html>
 '''
