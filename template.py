9191927253bb453fdcc5518d3a753296774ee2c0
anjiwon319 <anjiwon319@kookmin.ac.kr>
2020-06-28 02:32:28 +0900

    issue #4 make calc directory and wsgi files about calc

---
+++ template.py
@@ -0,0 +1,14 @@
+html = b'''
+<html>
+    <body>
+        <form action="">
+            A = <input type="number" name="a"><br><br>
+            B = <input type="number" name="b"><br><br>
+            <input type="submit">
+            <br><br> Add = %(Add)s<br>
+            <br><br> Multiply = %(Multiply)s<br>
+        </form>
+    </body>
+</html>
+'''
+
