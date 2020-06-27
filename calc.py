1aaaa575c1c161e5d3a417d91c358f237899b010
anjiwon319 <anjiwon319@kookmin.ac.kr>
2020-06-28 05:51:20 +0900

    issue #7 modified default: a, b = 0, 0 -> add, mul = 0, 0

--- calc.py
+++ calc.py
@@ -3,16 +3,14 @@ from TEMPLATE import html
 
 def application(environ, start_response):
     d = parse_qs(environ['QUERY_STRING'])
-    a = d.get('a',['0'])[0]
-    b = d.get('b',['0'])[0]
-    
+    a = d.get('a',[''])[0]
+    b = d.get('b',[''])[0]
+    add, mul = 0, 0
     if '' not in [a, b]:
         a, b = int(a), int(b)
-    
-    response_body = html % {
-            'Add': a+b, 'Multiply': a*b
-            }
-
+        add = a + b
+        mul = a * b
+    response_body = html % {'add':add, 'mul':mul}
     start_response('200 OK', [
         ('Content-Type', 'text/html'),
         ('Content-Length', str(len(response_body)))
